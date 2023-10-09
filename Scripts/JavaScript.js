9dd77b1c396f8267c7af7141d34478f60267bb7b
Dale Hunt <dalehunt@gmail.com>
2023-10-09 23:09:42 +0100

    Added more names. Removed `!` from Welcome Text.

--- Scripts/JavaScript.js
+++ Scripts/JavaScript.js
@@ -1,4 +1,16 @@
-var Usernames = ["Dale", "Artemi", "Beck", "Bob", "Emma"];
+var Usernames = [
+  "Dale",
+  "Artemi",
+  "Beck",
+  "Bob",
+  "Emma",
+  "Gina",
+  "Henry",
+  "Isaac",
+  "Jasmine",
+  "Kai",
+];
+
 var RandomizeIndex = Math.floor(Math.random() * Usernames.length);
 var RandomName = Usernames[RandomizeIndex];
 document.querySelector("#Username").textContent = RandomName;
